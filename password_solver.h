7e50fc8174ede305336650270e61f45a95a0ebb0
Jake Cirino <jc767615@ohio.edu>
2019-03-27 19:58:10 -0400

    Completed permuation function

--- password_solver.h
+++ password_solver.h
@@ -11,8 +11,8 @@ using namespace std;
 #include <vector>
 #include <string>
 #include <unistd.h>
-#include <tgmath.h>
 #include <sstream>
+#include <iostream>
 
 class password_solver{
 
@@ -21,42 +21,64 @@ public:
         this->salts = salts;
         this->encoded = encoded;
         this->dictionary = dictionary;
-        max_num = pow(10, max_digits)-1;
+        max_num = power(10, max_digits)-1;
     }
 
-    vector<string> solve_passwords(){
+    void solve_passwords(){
         //create new vector to store results
         vector<string> passwords(encoded.size());
+        int dic_size = dictionary.size();
 
-        //loop through num of words, start of permutation
-        int passes_found = 0;
-        while(passes_found < encoded.size()){
-            string cur_pass;
-            for(int words = 1; words <= 2; words++){
-                //determine number before word
-                string num1_str;
-                for(int num1 = -1; num1 <= max_num; num1++){
-                    if(num1 != -1){
-                        stringstream out;
-                        out << num1;
-                        num1_str = out.str();
+        //loop through number
+        for(int num = -1; num <= max_num; num++){
+            //first word
+            for(int word1i = 0; word1i < dic_size; word1i++){
+                string word1 = dictionary[word1i];
+                //second word, no second word at -1
+                for(int word2i = -1; word2i < dic_size; word2i++){
+                    string word2 = "";
+                    //get number position, 0 = before first, 1 = after first, 2 = after second
+                    int pos_max = 2;
+                    if(word2i == -1)
+                        pos_max = 1;
+                    else
+                        word2 = dictionary[word2i];
+                    //determine pass with/without num
+                    if(num != -1){
+                        //determine number position
+                        for(int num_pos = 0; num_pos <= pos_max; num_pos++){
+                            //complete pass
+                            string pass;
+
+                            //convert number to string
+                            string num_string;
+                            stringstream ss;
+                            ss << num;
+                            num_string = ss.str();
+
+                            if(num_pos == 0)
+                                pass += num_string;
+
+                            pass += word1;
+
+                            if(num_pos == 1)
+                                pass += num_string;
+
+                            pass += word2;
+
+                            if(num_pos == 2)
+                                pass += num_string;
+
+                            //TODO do shit
+                            cout << pass << endl;
+                        }
+                    }else{
+                        //there is no number, combine the words
+                        string pass = word1 + word2;
+
+                        //TODO do shit
+                        cout << pass << endl;
                     }
-                }
-
-                //determine number after word
-                string num2_str;
-                for(int num2 = -1; num2 <= max_num; num2++){
-                    if(num2 != -1){
-                        stringstream out;
-                        out << num2;
-                        num2_str = out.str();
-                    }
-                }
-
-                //loop through possible dictionary words for current word
-                string this_word;
-                for(int dic_val = 0; dic_val < dictionary.size(); dic_val++){
-                    this_word = dictionary[dic_val];
                 }
             }
         }
@@ -65,6 +87,13 @@ public:
 private:
     vector<string> salts, encoded, dictionary;
     int max_num;
+
+    int power(int base, int exp){
+        if(exp == 1)
+            return base;
+        else
+            return base * power(base, exp-1);
+    }
 };
 
 #endif
